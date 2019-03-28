bbc214fc7d5659cd55c9bbd05a6d0c2ffb745145
gonzalovazquez <gonzalovazquez010@gmail.com>
2019-03-28 01:09:46 -0400

    feat: 🎸 upload website via terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,4 @@
 
-variable "project_name" {
-  type = "string"
-  default = "personal-website-235123"
-}
-
 provider "google" {
   project = "${var.project_name}"
   region  = "us-east4"
@@ -12,20 +7,89 @@ provider "google" {
 
 # GOOGLE CLOUD STORAGE - Publish Website
 resource "google_storage_bucket" "usmakestwo" {
-  name = "www.usmakestwo.com"
+  name = "${var.website_domain}"
+  location = "US"
+  
+  website {
+    main_page_suffix = "index.html"
+  }
 }
 
-resource "google_storage_bucket_acl" "usmakestwo-acl" {
+resource "google_storage_bucket_iam_member" "member" {
   bucket = "${google_storage_bucket.usmakestwo.name}"
-
-  role_entity = [
-    "READER:allUsers"
-  ]
+  role        = "roles/storage.objectViewer"
+  member      = "allUsers"
 }
 
-resource "google_storage_bucket_object" "www" {
+resource "google_storage_bucket_object" "index" {
   name   = "index.html"
   storage_class = "MULTI_REGIONAL"
   bucket = "${google_storage_bucket.usmakestwo.name}"
   source = "../src/index.html"
+}
+
+resource "google_storage_bucket_object" "privacy" {
+  name   = "privacy.html"
+  storage_class = "MULTI_REGIONAL"
+  bucket = "${google_storage_bucket.usmakestwo.name}"
+  source = "../src/privacy.html"
+}
+
+# Stylesheet
+resource "google_storage_bucket_object" "css" {
+  name   = "styles/application.css"
+  storage_class = "MULTI_REGIONAL"
+  bucket = "${google_storage_bucket.usmakestwo.name}"
+  source = "../src/styles/application.css"
+  content_type = "text/css"
+}
+
+# Images
+resource "google_storage_bucket_object" "logo" {
+  name   = "images/UsMakesTwoLogo.png"
+  storage_class = "MULTI_REGIONAL"
+  bucket = "${google_storage_bucket.usmakestwo.name}"
+  source = "../src/images/UsMakesTwoLogo.png"
+}
+
+resource "google_storage_bucket_object" "sprite" {
+  name   = "images/sprite.png"
+  storage_class = "MULTI_REGIONAL"
+  bucket = "${google_storage_bucket.usmakestwo.name}"
+  source = "../src/images/sprite.png"
+}
+
+resource "google_storage_bucket_object" "bg" {
+  name   = "images/bg.jpeg"
+  storage_class = "MULTI_REGIONAL"
+  bucket = "${google_storage_bucket.usmakestwo.name}"
+  source = "../src/images/bg.jpeg"
+}
+
+resource "google_storage_bucket_object" "cibc" {
+  name   = "images/clients/CIBC_logo.png"
+  storage_class = "MULTI_REGIONAL"
+  bucket = "${google_storage_bucket.usmakestwo.name}"
+  source = "../src/images/clients/CIBC_logo.png"
+}
+
+resource "google_storage_bucket_object" "td" {
+  name   = "images/clients/td-bank-logo.png"
+  storage_class = "MULTI_REGIONAL"
+  bucket = "${google_storage_bucket.usmakestwo.name}"
+  source = "../src/images/clients/td-bank-logo.png"
+}
+
+resource "google_storage_bucket_object" "font_woff" {
+  name   = "fonts/font-awesome/fontawesome-webfont.woff"
+  storage_class = "MULTI_REGIONAL"
+  bucket = "${google_storage_bucket.usmakestwo.name}"
+  source = "../src/fonts/font-awesome/fontawesome-webfont.woff"
+}
+
+resource "google_storage_bucket_object" "font_ttf" {
+  name   = "fonts/font-awesome/fontawesome-webfont.ttf"
+  storage_class = "MULTI_REGIONAL"
+  bucket = "${google_storage_bucket.usmakestwo.name}"
+  source = "../src/fonts/font-awesome/fontawesome-webfont.ttf"
 }
